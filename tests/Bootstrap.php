41cc931e4ff0f811b3bb2b187bc3e37c4413ef6d
Rob van der Lee <EpicSoftworks@gmail.com>
2018-03-05 21:35:14 +0100

    Removed the coverage file, accidently uploaded that one :)

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -2,17 +2,9 @@
 
 ini_set('error_reporting', E_ALL | E_STRICT);
 
-$files = array(__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php');
+$loader = require __DIR__ . '/../vendor/autoload.php';
 
-foreach ($files as $file) {
-    if (file_exists($file)) {
-        $loader = require $file;
-
-        break;
-    }
-}
-
-if (! isset($loader)) {
+if (!isset($loader)) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
 
